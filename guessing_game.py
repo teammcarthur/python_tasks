ce3ebf310cc4fee08c8abef7e811145515d1f9e0
teammcarthur <chrism.23092@obhs.school.nz>
2025-08-04 12:15:32 +1200

    rename and polished program
    Stopped invalid inputs, and completed the requirements for the guessing game

---
+++ guessing_game.py
@@ -0,0 +1,31 @@
+'''
+Author: Chris
+Version: 1.0
+Description: This is a number guessing game
+'''
+import random
+name = input('Please enter your name: ')
+number = random.randint(1, 10)
+numberint = int(number)
+guesses = 0
+#print('the number is', numberint)
+while(True):
+    while(True):
+        guess = input("Please guess a number: ")
+        if guess.isnumeric():
+                break
+        else:
+            print('This guess is invalid')
+            continue
+    guessint = int(guess)
+    guesses = guesses + 1
+    if guessint == numberint:
+        print('You got it!')
+        break
+    if guessint > numberint:
+        print('You guessed too high, try again: ')
+        continue
+    if guessint < numberint:
+        print('You guessed too low, try again: ')
+        continue
+print(f'You guessed {guesses} times!')
--- lists_prac1.py
+++ lists_prac1.py
@@ -14,7 +14,7 @@ sports = ['Football', 'Rugby', 'Hockey', 'Basketball', 'Cricket', 'Volleyball']
 sports.append('Tennis')
 
 print(sports)
-shuffle(sports) #or random.shuffle if importted the regular way?????
+shuffle(sports) #or random.shuffle if imported the regular way?????
 print(sports)
 
 random_sport = choice(sports)
