287f9f33ed0a20dc551a0ae9c82e5697ca3102ff
teammcarthur <chrism.23092@obhs.school.nz>
2025-08-04 12:28:28 +1200

    put code in function

--- guessing_game.py
+++ guessing_game.py
@@ -3,29 +3,33 @@ Author: Chris
 Version: 1.0
 Description: This is a number guessing game
 '''
-import random
-name = input('Please enter your name: ')
-number = random.randint(1, 10)
-numberint = int(number)
-guesses = 0
-#print('the number is', numberint)
-while(True):
+def guessing():
+    import random
+    name = input('Please enter your name: ')
+    number = random.randint(1, 10)
+    numberint = int(number)
+    guesses = 0
+    #print('the number is', numberint)
     while(True):
-        guess = input('Please guess a number: ')
-        if guess.isnumeric():
-                break
-        else:
-            print('This guess is invalid')
+        while(True):
+            guess = input('Please guess a number: ')
+            if guess.isnumeric():
+                    break
+            else:
+                print('This guess is invalid')
+                continue
+        guessint = int(guess)
+        guesses = guesses + 1
+        if guessint == numberint:
+            print('You got it!')
+            break
+        if guessint > numberint:
+            print('You guessed too high, try again: ')
             continue
-    guessint = int(guess)
-    guesses = guesses + 1
-    if guessint == numberint:
-        print('You got it!')
-        break
-    if guessint > numberint:
-        print('You guessed too high, try again: ')
-        continue
-    if guessint < numberint:
-        print('You guessed too low, try again: ')
-        continue
-print(f'You guessed {guesses} times!')
+        if guessint < numberint:
+            print('You guessed too low, try again: ')
+            continue
+    print(f'You guessed {guesses} times!')
+
+#-----main program------
+guessing()
