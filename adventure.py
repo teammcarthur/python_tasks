8581b1a4966dbed363545f50c28687b435ef5f2b
teammcarthur <chrism.23092@obhs.school.nz>
2025-08-15 10:44:11 +1200

    user input validation done

--- adventure.py
+++ adventure.py
@@ -5,7 +5,7 @@ version: 1.0
 description: This is battle code
 '''
 #----libraries------------
-
+import random #lets you use randint and shuffle, along with other random modules
 
 #---functions-------------
 
@@ -20,17 +20,21 @@ if __name__ == '__main__':
     en_dff = 13 #enemys defence
 
     while(hp > 0 and en_hp > 0):
-        while(True)
-            attack = input('Please enter your attack [s]word [a]xe [h]eal: ')
-            if(attack == 'a' or attack == 's' or attack == 'h'):
-                break
-            else:
-                print('This is not a valid input, please enter a, s, or h')
-                continue
+        #This loop checks that the user has entered a valid input - a, s, or h.
+        while(True):
+            try:
+                attack = input('Please enter your attack [s]word [a]xe [h]eal: ') #makes the user enter something
+                if(attack[0].lower() == 'a' or attack[0].lower == 's' or attack[0].lower() == 'h'): #checks that they entered a valid option
+                    print('test')
+                    break
+            except:
+                    print('This is not a valid input, please enter a, s, or h') #makes them try again if they entered something invalid
+                    continue
 
-        if(attack == 's'):
+        if(attack == 's'): #if they chose sword, this runs sword
+            print('sword')
             pass
-        elif(attack == 'a'):
+        elif(attack == 'a'): #If they chose axe, this runs axe
             pass
-        elif(attack == 'h'):
+        elif(attack == 'h'): #If they chose heal, this runs heal
             pass
