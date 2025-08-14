3db4ae44012f14d017c6c47d6bf4d5d472140e6b
teammcarthur <chrism.23092@obhs.school.nz>
2025-08-15 10:09:04 +1200

    adventure code framework

---
+++ adventure.py
@@ -0,0 +1,16 @@
+'''
+author: Chris
+date: 15/08/25
+version: 1.0
+description: This is battle code
+'''
+#----libraries------------
+
+
+#---functions-------------
+
+
+#---main-----------
+if __name__ == '__main__':
+    while(True):
+        letter = input('Please enter a letter: ')
